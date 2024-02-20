a3c9d9f911059b5d50efe8ec4581124b1d7a2f55
Mesww <123399888+Mesww@users.noreply.github.com>
2024-02-20 22:07:16 +0700

    make it better

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -3,6 +3,8 @@ class UserService {
     static async registerUser(studentid: any,password: any){
         try {
             // ! create user by real_users_model
+            
+
             const createUser = new User({studentid,password,role:'student'});
             return await createUser.save();
         } catch (error) {
